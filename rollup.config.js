4d3c31027a40250e6b9f86ec2a5f27859881933a
corentinleberre <corentin.leb29@gmail.com>
2021-11-20 15:14:08 +0100

    :heavy_plus_sign: Add Terser dependencies to minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
     input: 'src/index.js',
+    plugins: [
+      terser()
+    ],
     output: {
       file: 'dist/docsify-replace-pattern.js',
       format: 'cjs'
